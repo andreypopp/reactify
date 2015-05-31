d73901dc12bb22fd265e6f3dc023c3c55cbd810d
Maksim Horbachevsky <fantactuka@gmail.com>
2015-05-31 14:14:54 +0300

    Precompile matcher and use relative name for it

--- index.js
+++ index.js
@@ -6,11 +6,12 @@
 var ReactTools = require('react-tools');
 var through    = require('through');
 var minimatch  = require('minimatch');
-var flatten    = require('flatten');
+var path       = require('path')
 
 function reactify(filename, options) {
   options = options || {};
 
+  var exclude = computeExcluded(options.exclude);
   var buf = [];
 
   function write(chunk) {
@@ -23,7 +24,7 @@ function reactify(filename, options) {
   function compile() {
     var source = Buffer.concat(buf).toString();
     // jshint -W040
-    if (isJSXFile(filename, options)) {
+    if (isJSXFile(filename, exclude, options)) {
       try {
         var output = ReactTools.transform(source, {
           es5: options.target === 'es5',
@@ -49,9 +50,11 @@ function reactify(filename, options) {
   return through(write, compile);
 }
 
-function isJSXFile(filename, options) {
-  if (options.exclude && flatten([options.exclude]).some(function (pattern) {
-    return minimatch(filename, pattern, { matchBase: true });
+function isJSXFile(filename, exclude, options) {
+  var relativeName = path.relative(process.cwd(), filename);
+
+  if (exclude.some(function(matcher) {
+    return matcher.match(relativeName);
   })) {
     return false;
   }
@@ -66,6 +69,16 @@ function isJSXFile(filename, options) {
       .map(function(ext) { return ext[0] === '.' ? ext.slice(1) : ext });
     return new RegExp('\\.(' + extensions.join('|') + ')$').exec(filename);
   }
+}
+
+function computeExcluded(excluded) {
+  if (!excluded) {
+    return [];
+  }
+
+  return (Array.isArray(excluded) ? excluded : [excluded]).map(function (pattern) {
+    return minimatch.Minimatch(pattern);
+  });
 }
 
 module.exports = reactify;
